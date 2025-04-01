b0cf7d846ecfdc8fd3fe2e20d23074682e3d8221
Ratinan Lee <lee.ratinan@gmail.com>
2025-04-01 08:37:10 +0800

    img optimization

--- app/Views/home.php
+++ app/Views/home.php
@@ -15,11 +15,11 @@ $this->section('content');
         </div>
     </section><!-- /Hero Section -->
     <!-- About Section -->
-    <section id="about" class="about section">
+    <section id="about" class="about section orange-background text-white">
         <div class="container">
             <div class="row gy-4">
                 <div class="col-12" data-aos="fade-up" data-aos-delay="100">
-                    <h2><?= lang('Home.intro.title') ?></h2>
+                    <h2 class="text-white"><?= lang('Home.intro.title') ?></h2>
                 </div>
                 <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
                     <img src="<?= base_url('img/home-intro.jpg') ?>" class="img-fluid rounded-4 mb-4" alt="<?= lang('Home.hero.title') ?>">
@@ -38,7 +38,7 @@ $this->section('content');
         </div>
     </section><!-- /About Section -->
     <!-- Why Us Section -->
-    <section id="carousel" class="testimonials section dark-background">
+    <section id="carousel" class="testimonials section">
         <div class="container" data-aos="fade-up" data-aos-delay="200">
             <div class="row">
                 <div class="col-12">
@@ -57,7 +57,7 @@ $this->section('content');
                             <?php foreach ($why as $i => $value) : ?>
                                 <div class="swiper-slide">
                                     <div class="testimonial-item">
-                                        <img src="<?= base_url('img/why-academy-' . $i . '.jpg') ?>" class="img-fluid" alt="<?= lang('Home.why-academy.title') ?> <?= $i ?>">
+                                        <img src="<?= base_url('img/why-academy-' . $i . '.jpg') ?>" class="img-fluid img-thumbnail rounded-4" alt="<?= lang('Home.why-academy.title') ?> <?= $i ?>">
                                         <p class="mt-4"><?= $value ?></p>
                                     </div>
                                 </div><!-- End testimonial item -->
@@ -69,26 +69,25 @@ $this->section('content');
             </div>
         </div>
     </section>
-    <section id="carousel" class="testimonials section">
+    <section id="carousel" class="testimonials section orange-background">
         <div class="container" data-aos="fade-up" data-aos-delay="300">
-            <div class="row">
-                <div class="col-12">
+            <div class="row d-flex">
+                <div class="col-md-6 text-center px-5 pb-5 text-white">
+                    <h2 class="mb-5"><?= lang('Home.why-trackman.title') ?></h2>
+                    <img src="<?= base_url('img/home-trackman.png') ?>" class="img-fluid" alt="<?= lang('Home.why-trackman.title') ?>" />
+                </div>
+                <div class="col-md-6">
                     <div class="swiper init-swiper">
                         <script type="application/json" class="swiper-config">
                             {"loop": true, "speed": 600, "autoplay": {"delay": 5000}, "slidesPerView": "auto",
                                 "pagination": {"el": ".swiper-pagination-why-trackman", "type": "bullets", "clickable": true}}
                         </script>
-                        <div class="row">
-                            <div class="col text-center my-3">
-                                <h2><?= lang('Home.why-trackman.title') ?></h2>
-                            </div>
-                        </div>
                         <div class="swiper-wrapper">
                             <?php $why = lang('Home.why-trackman.items') ?>
                             <?php foreach ($why as $i => $value) : ?>
                                 <div class="swiper-slide">
                                     <div class="testimonial-item">
-                                        <img src="<?= base_url('img/why-trackman-' . $i . '.jpg') ?>" class="img-fluid" alt="<?= lang('Home.why-trackman.title') ?> <?= $i ?>">
+                                        <img src="<?= base_url('img/why-trackman-' . $i . '.jpg') ?>" class="img-fluid img-thumbnail rounded-4" alt="<?= lang('Home.why-trackman.title') ?> <?= $i ?>">
                                         <p class="mt-4"><?= $value ?></p>
                                     </div>
                                 </div><!-- End testimonial item -->
