2134fa00a8810d1171b6b79e5c20bc36b042def1
Ratinan Lee <lee.ratinan@gmail.com>
2025-04-03 08:48:23 +0800

    review page

--- app/Views/reviews.php
+++ app/Views/reviews.php
@@ -3,40 +3,33 @@ $this->extend('_layout.php');
 $this->section('content');
 ?>
     <h1 class="d-none"><?= lang('Theme.navigations.reviews') ?></h1>
-    <section id="contact" class="contact section mt-5 pt-5">
+    <section id="contact" class="contact section mt-5 pt-5 pb-2">
         <!-- Section Title -->
         <div class="container section-title mt-5 pb-3" data-aos="fade-up">
             <h2><span class="d-none"><?= lang('Reviews.title') ?></span> <i class="fa-solid fa-chevron-right"></i><i class="fa-solid fa-chevron-right"></i></h2>
             <p><?= lang('Reviews.title') ?></p>
         </div><!-- End Section Title -->
     </section>
-    <section id="testimonials" class="testimonials section light-background">
-        <img src="<?= base_url('img/reviews-bg.jpg') ?>" class="testimonials-bg" alt="<?= lang('Theme.navigations.reviews') ?>">
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-            <div class="swiper init-swiper">
-                <script type="application/json" class="swiper-config">
-                    {"loop": true, "speed": 600, "autoplay": {"delay": 5000}, "slidesPerView": "auto", "pagination": {"el": ".swiper-pagination", "type": "bullets", "clickable": true}}
-                </script>
-                <div class="swiper-wrapper">
-                    <?php $reviews = lang('Reviews.reviews') ?>
-                    <?php foreach ($reviews as $review) : ?>
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <i class="fa-solid fa-circle-user fa-3x"></i>
-                            <h2 class="mt-3"><?= $review['name'] ?></h2>
-                            <div class="stars">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                            </div>
-                            <p><?= $review['message'] ?></p>
+    <div class="container-fluid text-center" data-aos="fade-up" data-aos-delay="100">
+        <img src="<?= base_url('img/reviews-background.png') ?>" class="img-fluid w-100" alt="<?= lang('Theme.navigations.reviews') ?>">
+    </div>
+    <section class="section light-background mt-4">
+        <div class="container">
+            <div class="row">
+                <?php $reviews = lang('Reviews.reviews') ?>
+                <?php foreach ($reviews as $i => $review) : ?>
+                    <div class="col-12 <?= (0 == $i % 2 ? '':'text-md-end') ?>"  data-aos="fade-up" data-aos-delay="<?= ($i+1)*150 ?>">
+                        <h2 class="mt-3"><?= $review['name'] ?></h2>
+                        <div class="text-warning">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
                         </div>
-                    </div><!-- End testimonial item -->
-                    <?php endforeach; ?>
-                </div>
-                <div class="swiper-pagination"></div>
+                        <p class="my-3"><?= $review['message'] ?></p>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </section>
