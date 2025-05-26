421e448f0e93243a71c496abcf643cb98e4fa44c
Ratinan Lee <lee.ratinan@gmail.com>
2025-05-26 11:51:14 +0800

    fix canonical

--- app/Views/_layout.php
+++ app/Views/_layout.php
@@ -45,7 +45,7 @@
     <link rel="alternate" hreflang="th" href="<?= base_url('th/' . $handle_nav) ?>">
     <link rel="alternate" hreflang="ja" href="<?= base_url('ja/' . $handle_nav) ?>">
     <link rel="alternate" hreflang="x-default" href="<?= base_url($handle_nav) ?>">
-    <link rel="canonical" href="<?= base_url($handle_nav) ?>">
+    <link rel="canonical" href="<?= str_replace('index.php/', '', current_url()) ?>">
     <?php endif; ?>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?= getenv('ANALYTICS_ID') ?>"></script>
